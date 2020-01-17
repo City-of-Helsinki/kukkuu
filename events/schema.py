bfd7239a9472e44207b39933baeb421ceaea485a
Khin Loc <khin.loc@anders.fi>
2020-01-17 17:26:19 +0200

    Add mutation API for create events/venues/occurrences

--- events/schema.py
+++ events/schema.py
@@ -1,11 +1,15 @@
 import graphene
 from django.apps import apps
+from django.db import transaction
 from graphene import relay
 from graphene_django import DjangoConnectionField, DjangoObjectType
-from graphql_jwt.decorators import login_required
+from graphql_jwt.decorators import login_required, staff_member_required
+from graphql_relay import from_global_id
 
 from events.models import Event, Occurrence
-from venues.schema import VenueNode
+from kukkuu.exceptions import KukkuuGraphQLError
+from venues.models import Venue
+from venues.schema import VenueInput, VenueNode
 
 EventTranslation = apps.get_model("events", "EventTranslation")
 
@@ -53,9 +57,80 @@ class OccurrenceNode(DjangoObjectType):
         interfaces = (relay.Node,)
 
 
+class EventTranslationsInput(graphene.InputObjectType):
+    name = graphene.String(required=True)
+    short_description = graphene.String()
+    description = graphene.String()
+    language_code = graphene.String()
+
+
+class EventInput(graphene.InputObjectType):
+    id = graphene.GlobalID()
+    translations = graphene.List(EventTranslationsInput)
+    duration = graphene.Int()
+    participants_per_invite = graphene.String()
+    capacity_per_occurrence = graphene.Int()
+
+
+class AddEventMutation(graphene.relay.ClientIDMutation):
+    class Input:
+        translations = graphene.List(EventTranslationsInput)
+        duration = graphene.Int()
+        participants_per_invite = graphene.String(required=True)
+        capacity_per_occurrence = graphene.Int(required=True)
+        published_at = graphene.DateTime()
+
+    event = graphene.Field(EventNode)
+
+    @classmethod
+    @staff_member_required
+    @transaction.atomic
+    def mutate_and_get_payload(cls, root, info, **kwargs):
+        # TODO: Add validation
+        event = Event.objects.create_translatable_object(**kwargs)
+        # TODO: Add support for image upload
+        return AddEventMutation(event=event)
+
+
+class AddOccurrenceMutation(graphene.relay.ClientIDMutation):
+    class Input:
+        time = graphene.DateTime()
+        event = EventInput()
+        venue = VenueInput()
+
+    occurrence = graphene.Field(OccurrenceNode)
+
+    @classmethod
+    @staff_member_required
+    @transaction.atomic
+    def mutate_and_get_payload(cls, root, info, **kwargs):
+        # TODO: Validate data
+        event_data = kwargs.pop("event")
+        venue_data = kwargs.pop("venue")
+        try:
+            event_id = from_global_id(event_data.pop("id"))[1]
+            event = Event.objects.get(pk=event_id)
+        except Event.DoesNotExist as e:
+            raise KukkuuGraphQLError(e)
+
+        try:
+            venue_id = from_global_id(venue_data.pop("id"))[1]
+            venue = Venue.objects.get(pk=venue_id)
+        except Venue.DoesNotExist as e:
+            raise KukkuuGraphQLError(e)
+
+        occurrence = Occurrence.objects.create(**kwargs, event=event, venue=venue)
+        return AddOccurrenceMutation(occurrence=occurrence)
+
+
 class Query:
     events = DjangoConnectionField(EventNode)
     occurrences = DjangoConnectionField(OccurrenceNode)
 
     event = relay.Node.Field(EventNode)
     occurrence = relay.Node.Field(OccurrenceNode)
+
+
+class Mutation:
+    add_event = AddEventMutation.Field()
+    add_occurrence = AddOccurrenceMutation.Field()
--- events/tests/test_api.py
+++ events/tests/test_api.py
@@ -3,6 +3,7 @@ from graphql_relay import to_global_id
 
 from common.tests.utils import assert_permission_denied
 from events.factories import EventFactory, OccurrenceFactory
+from venues.factories import VenueFactory
 
 
 @pytest.fixture(autouse=True)
@@ -153,6 +154,69 @@ query Occurrence($id: ID!) {
 }
 """
 
+ADD_EVENT_MUTATION = """
+mutation AddEvent($input: AddEventMutationInput!) {
+  addEvent(input: $input) {
+    event {
+      translations{
+        languageCode
+        name
+        description
+        shortDescription
+      }
+      duration
+      image
+      participantsPerInvite
+      capacityPerOccurrence
+      publishedAt
+    }
+  }
+}
+"""
+
+ADD_EVENT_VARIABLES = {
+    "input": {
+        "translations": [
+            {
+                "name": "Event test",
+                "shortDescription": "Short desc",
+                "description": "desc",
+                "languageCode": "fi",
+            }
+        ],
+        "duration": 1000,
+        # "image": None,
+        "participantsPerInvite": "family",
+        "capacityPerOccurrence": 30,
+        "publishedAt": "1986-12-12T16:40:48+00:00",
+    }
+}
+
+ADD_OCCURRENCE_MUTATION = """
+mutation AddOccurrence($input: AddOccurrenceMutationInput!) {
+  addOccurrence(input: $input) {
+    occurrence{
+      event{
+        id,
+      }
+      venue {
+        id
+      }
+        time
+    }
+  }
+}
+
+"""
+
+ADD_OCCURRENCE_VARIABLES = {
+    "input": {
+        "event": {"id": ""},
+        "venue": {"id": ""},
+        "time": "1986-12-12T16:40:48+00:00",
+    }
+}
+
 
 def test_events_query_unauthenticated(api_client):
     executed = api_client.execute(EVENTS_QUERY)
@@ -211,4 +275,48 @@ def test_occurrence_query_normal_user(snapshot, user_api_client):
     variables = {"id": to_global_id("OccurrenceNode", occurrence.id)}
     executed = user_api_client.execute(OCCURRENCE_QUERY, variables=variables)
 
+    snapshot.assert_match(executed)
+
+
+def test_add_event_permission_denied(api_client, user_api_client):
+    executed = api_client.execute(ADD_EVENT_MUTATION, variables=ADD_EVENT_VARIABLES)
+    assert_permission_denied(executed)
+
+    executed = user_api_client.execute(
+        ADD_EVENT_MUTATION, variables=ADD_EVENT_VARIABLES
+    )
+    assert_permission_denied(executed)
+
+
+def test_add_event_staff_user(snapshot, staff_api_client):
+    executed = staff_api_client.execute(
+        ADD_EVENT_MUTATION, variables=ADD_EVENT_VARIABLES
+    )
+    snapshot.assert_match(executed)
+
+
+def test_add_occurrence_permission_denied(api_client, user_api_client):
+    executed = api_client.execute(
+        ADD_OCCURRENCE_MUTATION, variables=ADD_OCCURRENCE_VARIABLES
+    )
+    assert_permission_denied(executed)
+
+    executed = user_api_client.execute(
+        ADD_OCCURRENCE_MUTATION, variables=ADD_OCCURRENCE_VARIABLES
+    )
+    assert_permission_denied(executed)
+
+
+def test_add_occurrence_staff_user(snapshot, staff_api_client):
+    event = EventFactory()
+    venue = VenueFactory()
+    ADD_OCCURRENCE_VARIABLES["input"]["event"]["id"] = to_global_id(
+        "EventNode", event.id
+    )
+    ADD_OCCURRENCE_VARIABLES["input"]["venue"]["id"] = to_global_id(
+        "VenueNode", venue.id
+    )
+    executed = staff_api_client.execute(
+        ADD_OCCURRENCE_MUTATION, variables=ADD_OCCURRENCE_VARIABLES
+    )
     snapshot.assert_match(executed)
