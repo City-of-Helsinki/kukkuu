ad946ff7f70764fc2fc0941d826e4b7703099c8d
Niko Lindroos <niko.j.makela@gmail.com>
2022-05-18 15:09:12 +0300

    Added tests for Event's and EventGroup's can_child_enroll -methods
    KK-930

--- events/tests/test_models.py
+++ events/tests/test_models.py
@@ -5,11 +5,17 @@ from django.core.exceptions import ValidationError
 from django.utils.timezone import now
 
 from children.factories import ChildFactory
+from projects.factories import ProjectFactory
 from projects.models import Project
 from venues.models import Venue
 
-from ..factories import EnrolmentFactory, EventFactory, OccurrenceFactory
-from ..models import Enrolment, Event, Occurrence
+from ..factories import (
+    EnrolmentFactory,
+    EventFactory,
+    EventGroupFactory,
+    OccurrenceFactory,
+)
+from ..models import Enrolment, Event, EventGroup, Occurrence
 
 User = get_user_model()
 
@@ -61,3 +67,213 @@ def test_enrolment_reference_id():
     for enrolment in enrolments:
         assert len(enrolment.reference_id) == settings.KUKKUU_HASHID_MIN_LENGTH
         assert Enrolment.decode_reference_id(enrolment.reference_id) == enrolment.id
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "has_enrolled,can_child_enroll", [(True, False), (False, True)]
+)
+def test_event_group_can_child_enroll_already_enrolled(
+    has_enrolled, can_child_enroll, child_with_random_guardian, future
+):
+    """Enrolment shouldn't be allowed since child has enrolled to a different event
+    in the same event group.
+    """
+    event_group = EventGroupFactory(
+        name="Event group with one of two events enrolled", published_at=now()
+    )
+    OccurrenceFactory(
+        time=future,
+        event__published_at=now(),
+        event__event_group=event_group,
+    )
+    enrolled_occurrence = OccurrenceFactory(
+        time=future,
+        event__published_at=now(),
+        event__event_group=event_group,
+    )
+    if has_enrolled:
+        EnrolmentFactory(
+            child=child_with_random_guardian, occurrence=enrolled_occurrence
+        )
+    assert event_group.can_child_enroll(child_with_random_guardian) is can_child_enroll
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "is_published,can_child_enroll",
+    [(True, True), (False, False)],
+)
+def test_event_group_can_child_enroll_unpublished(
+    is_published, can_child_enroll, child_with_random_guardian, future
+):
+    """Enrolment shouldn't be allowed since the event group is unpublished"""
+    event_group = EventGroupFactory(
+        name="Event group with an occurrence but might not be published yet",
+        published_at=now() if is_published else None,
+    )
+    OccurrenceFactory(
+        time=future,
+        event__published_at=now(),
+        event__event_group=event_group,
+    )
+    assert event_group.can_child_enroll(child_with_random_guardian) is can_child_enroll
+
+
+@pytest.mark.django_db
+def test_event_group_can_child_enroll_no_occurrence(child_with_random_guardian):
+    event_group = EventGroupFactory(
+        name="Event group without any occurrences", published_at=now()
+    )
+    assert event_group.can_child_enroll(child_with_random_guardian) is False
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "enrolment_limit,can_child_enroll",
+    [(3, True), (2, False), (1, False), (0, False)],
+)
+def test_event_group_can_child_enroll_project_limit_reached(
+    enrolment_limit, can_child_enroll, child_with_random_guardian, future
+):
+    """
+    Enrolment shouldn't be possible when event group enrolment limit is reached.
+    If the child's enrolments count in this year is lower
+    than the limit set in the child's project, enrolment should be possible.
+    """
+    project = ProjectFactory(enrolment_limit=enrolment_limit)
+    child_with_random_guardian.project = project
+    child_with_random_guardian.save()
+
+    for _ in range(2):
+        enrolled_event_group = EventGroupFactory(
+            name="Event group where the child has already enrolled",
+            published_at=now(),
+            project=project,
+        )
+        enrolled_occurrence = OccurrenceFactory(
+            time=future,
+            event__published_at=now(),
+            event__event_group=enrolled_event_group,
+        )
+        EnrolmentFactory(
+            child=child_with_random_guardian, occurrence=enrolled_occurrence
+        )
+
+    assert EventGroup.objects.count() == 2
+    assert Enrolment.objects.count() == 2
+    event_group = EventGroupFactory(
+        name="Event group with an occurrence", published_at=now(), project=project
+    )
+    OccurrenceFactory(
+        time=future,
+        event__published_at=now(),
+        event__event_group=event_group,
+    )
+    assert event_group.can_child_enroll(child_with_random_guardian) is can_child_enroll
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "is_published,can_child_enroll",
+    [(True, True), (False, False)],
+)
+def test_event_can_child_enroll_unpublished(
+    is_published, can_child_enroll, child_with_random_guardian, future
+):
+    """Enrolment shouldn't be allowed since the event is unpublished"""
+    event_group = EventGroupFactory(
+        published_at=now(),
+    )
+    occurrence = OccurrenceFactory(
+        time=future,
+        event__published_at=now() if is_published else None,
+        event__event_group=event_group,
+    )
+    assert (
+        occurrence.event.can_child_enroll(child_with_random_guardian)
+        is can_child_enroll
+    )
+
+
+@pytest.mark.django_db
+def test_event_can_child_enroll_no_occurrence(child_with_random_guardian):
+    """Enrolment shouldn't be allowed since it has no occurrences"""
+    event_group = EventGroupFactory(
+        published_at=now(),
+    )
+    event = EventFactory(published_at=now(), event_group=event_group)
+    assert event.can_child_enroll(child_with_random_guardian) is False
+
+
+@pytest.mark.django_db
+def test_event_can_child_enroll_event_group_unenrollable(child_with_random_guardian):
+    """Enrolment shouldn't be allowed since the event group is rejected"""
+    event_group = EventGroupFactory(
+        name="Unpublished Event Group",
+        published_at=None,
+    )
+    event = EventFactory(published_at=now(), event_group=event_group)
+    assert event.can_child_enroll(child_with_random_guardian) is False
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "enrolment_limit,can_child_enroll",
+    [(3, True), (2, False), (1, False), (0, False)],
+)
+def test_event_can_child_enroll_project_limit_reached(
+    enrolment_limit, can_child_enroll, child_with_random_guardian, future
+):
+    """
+    Enrolment shouldn't be possible when event enrolment limit is reached.
+    If the child's enrolments count in this year is lower
+    than the limit set in the child's project, enrolment should be possible.
+    """
+    project = ProjectFactory(enrolment_limit=enrolment_limit)
+    child_with_random_guardian.project = project
+    child_with_random_guardian.save()
+
+    for _ in range(2):
+        enrolled_event_group = EventGroupFactory(
+            name="Event group where the child has already enrolled",
+            published_at=now(),
+            project=project,
+        )
+        enrolled_occurrence = OccurrenceFactory(
+            time=future,
+            event__published_at=now(),
+            event__event_group=enrolled_event_group,
+        )
+        EnrolmentFactory(
+            child=child_with_random_guardian, occurrence=enrolled_occurrence
+        )
+
+    assert EventGroup.objects.count() == 2
+    assert Enrolment.objects.count() == 2
+    event_group = EventGroupFactory(
+        name="Event group with an occurrence", published_at=now(), project=project
+    )
+    occurrence = OccurrenceFactory(
+        time=future,
+        event__published_at=now(),
+        event__event_group=event_group,
+    )
+    assert (
+        occurrence.event.can_child_enroll(child_with_random_guardian)
+        is can_child_enroll
+    )
+
+
+@pytest.mark.django_db
+def test_event_can_child_enroll_already_enrolled(child_with_random_guardian, future):
+    event_group = EventGroupFactory(published_at=now())
+    enrolled_occurrence = OccurrenceFactory(
+        time=future,
+        event__published_at=now(),
+        event__event_group=event_group,
+    )
+    EnrolmentFactory(child=child_with_random_guardian, occurrence=enrolled_occurrence)
+    assert (
+        enrolled_occurrence.event.can_child_enroll(child_with_random_guardian) is False
+    )
