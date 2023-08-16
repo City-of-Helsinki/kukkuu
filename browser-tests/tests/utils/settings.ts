34f108bc431c2f642ae6fc16b55165fc89e71792
Tero Virtanen <tero.virtanen@mavericks.fi>
2023-08-16 11:18:31 +0300

    chore: browser tests api url might point to graphql

--- browser-tests/tests/utils/settings.ts
+++ browser-tests/tests/utils/settings.ts
@@ -16,11 +16,18 @@ function getOrError(variableName: string) {
   return variable;
 }
 
+const getApiBaseUrl = (url: string) => {
+  // API url might point to graphql, remove it from url
+  var re = /\/graphql$/;
+  return url.replace(re, "");
+};
+
+
 export const testUsername = (): string => getOrError('BROWSER_TESTS_API_ADMIN_USER_NAME');
 
 export const testUserPassword = (): string => getOrError('BROWSER_TESTS_API_ADMIN_PASSWORD');
 
-export const envUrl = (): string => getOrError('BROWSER_TESTS_API_URL');
+export const envUrl = (): string => getApiBaseUrl(getOrError('BROWSER_TESTS_API_URL'));
 
 export const testUiUsername = (): string => getOrError('BROWSER_TESTS_USER_NAME');
 
