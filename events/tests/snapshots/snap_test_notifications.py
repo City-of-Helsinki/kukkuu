622657b14a2adbd00d8974263037ed5e3d2365f9
Niko Lindroos <niko.j.makela@gmail.com>
2023-12-13 16:15:50 +0200

    test: update notification snapshot

--- events/tests/snapshots/snap_test_notifications.py
+++ events/tests/snapshots/snap_test_notifications.py
@@ -22,8 +22,8 @@ snapshots["test_event_group_republish_notification 1"] = [
         Guardian FI: Samantha Bryant (tonya77@example.com)
         Url: http://localhost:3000/fi/profile/child/Q2hpbGROb2RlOjAwMDAwMDAwLTAwMDAtMDAwMC0wMDAwLTAwMDAwMDAwMDMwOQ==/event-group/RXZlbnRHcm91cE5vZGU6Nzc3
         Events:
-            Girl middle same space speak. 2020-12-12 00:00:00+00:00 http://localhost:3000/fi/profile/child/Q2hpbGROb2RlOjAwMDAwMDAwLTAwMDAtMDAwMC0wMDAwLTAwMDAwMDAwMDMwOQ==/event/RXZlbnROb2RlOjc3OA==
             Base may middle good father boy economy. 2020-12-11 00:00:00+00:00 http://localhost:3000/fi/profile/child/Q2hpbGROb2RlOjAwMDAwMDAwLTAwMDAtMDAwMC0wMDAwLTAwMDAwMDAwMDMwOQ==/event/RXZlbnROb2RlOjc3Nw==
+            Girl middle same space speak. 2020-12-12 00:00:00+00:00 http://localhost:3000/fi/profile/child/Q2hpbGROb2RlOjAwMDAwMDAwLTAwMDAtMDAwMC0wMDAwLTAwMDAwMDAwMDMwOQ==/event/RXZlbnROb2RlOjc3OA==
 """
 ]
 
