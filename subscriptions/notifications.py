d86643c26c9ad8cd33533dba57f44031b8d4b1a9
Niko Lindroos <niko.j.makela@gmail.com>
2024-03-26 11:42:26 +0200

    fix: build in CI fails because of factory usage during build time
    KK-1072.
    ci pipeline does not handle the factory usage while build

--- subscriptions/notifications.py
+++ subscriptions/notifications.py
@@ -14,9 +14,7 @@ from projects.factories import ProjectFactory
 from subscriptions.consts import NotificationType
 from subscriptions.factories import FreeSpotNotificationSubscriptionFactory
 from users.factories import GuardianFactory
-from users.utils import get_marketing_unsubscribe_ui_url
 from venues.factories import VenueFactory
-from verification_tokens.factories import UserSubscriptionsAuthVerificationTokenFactory
 
 notifications.register(NotificationType.FREE_SPOT, _("free spot"))
 
@@ -29,10 +27,7 @@ occurrence = OccurrenceFactory.build(event=event, venue=venue)
 subscription = FreeSpotNotificationSubscriptionFactory.build(
     child=child, occurrence=occurrence
 )
-auth_verification_token = UserSubscriptionsAuthVerificationTokenFactory.build(
-    user=guardian.user
-)
-
+unsubscribe_url = "https://kukkuu-ui-domain/fi/profile/subscriptions?authToken=abc123"
 dummy_context.update(
     {
         NotificationType.FREE_SPOT: {
@@ -49,9 +44,7 @@ dummy_context.update(
             ),
             "subscription": subscription,
             "localtime": timezone.template_localtime,
-            "unsubscribe_url": get_marketing_unsubscribe_ui_url(
-                guardian, guardian.language, auth_verification_token
-            ),
+            "unsubscribe_url": unsubscribe_url,
         },
     }
 )
--- users/notifications.py
+++ users/notifications.py
@@ -2,12 +2,6 @@ from django.utils.translation import gettext_lazy as _
 from django_ilmoitin.dummy_context import dummy_context
 from django_ilmoitin.registry import notifications
 
-from users.utils import get_marketing_unsubscribe_ui_url
-from verification_tokens.factories import (
-    UserEmailVerificationTokenFactory,
-    UserSubscriptionsAuthVerificationTokenFactory,
-)
-
 from .factories import GuardianFactory
 
 
@@ -26,28 +20,19 @@ notifications.register(
 
 guardian = GuardianFactory.build()
 
-email_verification_token = UserEmailVerificationTokenFactory.build(user=guardian.user)
-
 # NOTE: Should the unsubscribe link be available in mandatory emails,
 # like transactional emails?
-auth_verification_token = UserSubscriptionsAuthVerificationTokenFactory.build(
-    user=guardian.user
-)
-
+unsubscribe_url = "https://kukkuu-ui-domain/fi/profile/subscriptions?authToken=abc123"
 dummy_context.update(
     {
         NotificationType.GUARDIAN_EMAIL_CHANGED: {
             "guardian": guardian,
-            "unsubscribe_url": get_marketing_unsubscribe_ui_url(
-                guardian, guardian.language, auth_verification_token
-            ),
+            "unsubscribe_url": unsubscribe_url,
         },
         NotificationType.GUARDIAN_EMAIL_CHANGE_TOKEN: {
             "guardian": guardian,
-            "verification_token": email_verification_token.key,
-            "unsubscribe_url": get_marketing_unsubscribe_ui_url(
-                guardian, guardian.language, auth_verification_token
-            ),
+            "verification_token": "aBcDXyZ123-",
+            "unsubscribe_url": unsubscribe_url,
         },
     }
 )
