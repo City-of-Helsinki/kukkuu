aee2fe0741feea616d64f8b2e4e653454dacff7f
Niko Lindroos <niko.j.makela@gmail.com>
2024-04-08 14:20:35 +0300

    test: add instructions to create a new PEM key pair
    KK-1073

--- gdpr/tests/keys.py
+++ gdpr/tests/keys.py
@@ -21,42 +21,74 @@ def _build_key(private_pem, public_pem):
     return key
 
 
-rsa_key = _build_key(
-    """-----BEGIN PRIVATE KEY-----
-MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDUeT3OFPatye6I
-tmArvjR+f0lZu4QEOxtGxHj1UWzLiUbONygTrWCVXh5OFaH+GFPOfqax2iJSWc+7
-6JYy2y2XxdG1Tehcvpsv/gKqRJG2afVKY+qCCmtRwksWan4kRU6F9UKDHJl6emkE
-e9xA/+DRVAE7cCM1xblI7YD3zXsf1xIWHjYfLS6/EJeVnq/bCN84LPuCO6148N2D
-H7E0PEH2oZyg0QlnU2tTcZMG2D172wFxyM8jGfy7Hm8X+MQH5N/aFeKb7f/6eTA7
-4a5hW5ncRTasaH5djCcBFSyAaS8F4UCZYsRdth1lp4nBi986DbL1uaVbumU2Oo6F
-a/K3uGRFAgMBAAECggEAAc5J/S9mbVGzCkxqgtSqA403ZWDXnWWXNMHEuWkIwK4Z
-APWtDIXDtWFIZqd+afdw9udSqV5OPl7vCgzPAf2k5I5U2vKfj/I6xWymPyY4CtHZ
-uNkijBpkkRxSoQ0kp1BDe5X7C7w5fbX+oIAg/hhuo7jQDd5FHlbg3ULPfsurSTj5
-wnaekK64DgTM6N/kLbFKMbRmyC54EfBqu/H3fnY3uljNZGFnL0ueyGB0NEij/f7P
-NXPzvi+r3Bmpe+K2P8mFBPNXhRIIUmfhxe19OcnkneGF3FTv+EqwzKid45/eSnC2
-a8SeH/1b9pMPg0WycNwIh4sz3K7w0/vwJVpMC3thgQKBgQDq5pImHasJCIEHw2bY
-A9NTGNP72ry8t96A6yLEa9w2i9lS7SMeUwuA5QdTE80+3FgC/S5YVxTyp0ceiUfr
-nELEFgqD+6wXao6CYaelcwligmockXrBDGWfC/QviMRh6zympCZC0MNQcGR61PMx
-tv8SqKi5ual+nT7PKqpbVygP5QKBgQDnjviEJa/E22VPoDZwnuwQCBf4fgQcOqSq
-j2SYBLdISPzNhkXJqqlQ7LKZsWQH+4YeDDL2gOnmyvvQiRXlhQSNiCE26RSv1E3f
-HiCvo8+AiDY49ywbbkL8E7sp5dSjmQhmQuRPolPMh2fJXvENxfEIXbwqpsCxxw0n
-F3NGlpn84QKBgAtvCbIdQ5P++/jaxAjDtueWj8r0jLdK4+O2jkytS1zEVeG5dTom
-pKqzezXKAvWKWCZdGIJoSra8+bM8z2lig8VzpTNjbq79Gs6x3i0pek13N58IXcdD
-yTaCqHIf4B88Cgm6d7pM2xTxQ5LPBr9mvuezmfLgXKWzFbmTxBMKHQMZAoGATb33
-e853U71hJzmf7XG9yagd/CS61otty4G3AT7cFh3DGnGRLqLok63UTLt83R06Kw5n
-cdFYNk9B+gJ8YoGlRKtGk3vvoRTDTDx+Ntnlib6xjbCWk2MShDVPqkJqgL6ZTlP4
-+S+DuPBhDP+eKMSjJu7phNxVZ5pvtQcvgayAaKECgYAsUa2jEZpLYwh2x+OW58YT
-6Po5IhRvBAiNJg/N7mirkdb7NyB6I4aA1ztoimizDrJPn7edVKUTluNw2Fk3QsLS
-va0XlEJu3URaqHLcKi6J74dlSt+3W4pSTJF7eseyFMI64bWSEho1tvChLSCq6lUE
-zyIWjEHazLOEdBArFsgWsg==
+def _get_pem_key_pair():
+    """Get a PEM-key pair.
+
+    NOTE: Follow the following instructions to create a new pair of PEM keys.
+
+    You can install ``cryptography`` with ``pip``:
+    - ``pip install cryptography``
+
+    The following script can be used to create a new pair of PEM keys.
+
+    >>> # doctest: +SKIP
+    ... from cryptography.hazmat.primitives.asymmetric import rsa
+    ... from cryptography.hazmat.primitives import serialization
+    ... private_key = rsa.generate_private_key(
+    ...     public_exponent=65537,
+    ...     key_size=2048
+    ... )
+    ... pem_private_key = private_key.private_bytes(
+    ...     encoding=serialization.Encoding.PEM,
+    ...     format=serialization.PrivateFormat.PKCS8,
+    ...     encryption_algorithm=serialization.NoEncryption()
+    ... )
+    ... pem_public_key = private_key.public_key().public_bytes(
+    ...   encoding=serialization.Encoding.PEM,
+    ...   format=serialization.PublicFormat.SubjectPublicKeyInfo
+    ... )
+    ... print(pem_private_key.decode("utf-8"))
+    ... print(pem_public_key.decode("utf-8"))
+    """
+    return (
+        """-----BEGIN PRIVATE KEY-----
+MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC/2Fgt2rRchA7t
+GP6FG3t9bcbQLPlZa3XEFTndv/snAaUBlP8rDiTvoToymNV82prOheYTdmN/sLP4
+a9c6nMcbilQjhkHGDZKxC9lOCakd4qL0iyfRO30IL7M5sX/sZP0tnAAOohEY1ljB
+MjNNmp45jIZE9LD0/fZ9pM1lv3BzURkKt/Diww8H9EfPG6Y5z44TYO0kfATUBF2z
+IZAJzgHAiIWS8BJ2lyidGGi2+Gx4K7f5fFmotjwMPYORiqbwpgSshVefUapedvJC
+VFGKPY6f+Beb6PzLE4kEK8+/ctnRdZt2pgbRrom776gUdg59OKL60EL5LExpb/kA
+DtRto/txAgMBAAECggEAMa9JHw8OOQumhfc8K6Lzd4ePvuh255ayGEdbBjgrRm3h
+myhIcZEnNbxuwx3b5IsFHsmEzbOSj0ZnRcZAJpjl5BcONWkW7cEkJaAo9lIAL5I7
+m9PSSxj6B7260A1NUR7ShxZo2WFVxjX1JIvox4dsxQDE4WTx03FWfjHJVDmhWOvL
+L0DRjXgTrYj1dBBEnUAWwkIlFDNFzqAVsiZHF+6XOuRUT+vEZPv02IkmUrn3uwos
+mBELDelOqL/m5t7EYkxW7apTuGXoxc8NOctDj10+OVcus0Kia8v1REJpkL2hF+AB
+gnAQSI8x6OeWjFOZuhUEUsSZLARnkT/K9EFxM4IOsQKBgQD8Ek5/SCAAUT7CFtmM
+MdZFpQ0I9ZjywtyrTZFY1g405HpGvzc+sBxu3+K1TPPO+GDz+zmHspaVKFw+4W6J
+i8oO6hf1H9N16xR+EqTMosSDX3ud8P1Awswu2TUdNMbCzqx2ICZauLjY1ArZnuPI
+BaJMwTY1z12wlZSgB8Gc7wIETQKBgQDC1cBp+5/yvbKoI2kUl/GABVaCumW7ieTz
+Yog31RwLR6mvef5LNFomONvDrNxKxSEHj3fo409ZoqNzArVGz3A0iK/CHKVjKpUp
+YMXOYMWBOHidC5D0wI3sb/UGoDj7CjCboBvvHbBCB+RVA5atuodLDUoshrVSNrgQ
+28JQBR81tQKBgQCHTqBaTHH5GaNxdeiDC8F0EvvjQko+jYD8Zx/NKuHnXHmSflP+
+T3SDw6QjI9J/1+3bKZChGakhGdAiZMn8BVCKHviLOPE+i9itL/7MZdbMmjV1+4VF
+/QqzXx7WtZy3t071/Z349s0qfu/wDw1AMl4Di3c4/T3SawijumYggl93xQKBgDjs
+EDImlJ1rtKWQ6uNcXO8lIBhDcvNunxhIYjnFplLZVgbxYk0Ad5IRLIunlhi2LScz
+UDoXJxit/ojccq/EbSi8AnV4vw0Q5NFY95GLDkjpgbuIJIqNMymvr+uGpf8aBAeD
+qIWcq+EuwxPfX4dUJrDTqicGGDVzzSUHv2Z3iJ29AoGBAK/8kuvvz7fl7nvR80zG
+xv4qkLETZPmGcssma1TskdNn8eYADcDgnnzI3Lldr6DoouGQ8eKNOUQC6X53GU12
+0k/+6od92VNGkQI8g7DCPwPC8cTPvvUGFkxg3J8v2nBcaLMmDVUaXrDfj+QG+jUB
+MG7YFa0LPJ8R79pFieleUm1O
 -----END PRIVATE KEY-----""",
-    """-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1Hk9zhT2rcnuiLZgK740
-fn9JWbuEBDsbRsR49VFsy4lGzjcoE61glV4eThWh/hhTzn6msdoiUlnPu+iWMtst
-l8XRtU3oXL6bL/4CqkSRtmn1SmPqggprUcJLFmp+JEVOhfVCgxyZenppBHvcQP/g
-0VQBO3AjNcW5SO2A9817H9cSFh42Hy0uvxCXlZ6v2wjfOCz7gjutePDdgx+xNDxB
-9qGcoNEJZ1NrU3GTBtg9e9sBccjPIxn8ux5vF/jEB+Tf2hXim+3/+nkwO+GuYVuZ
-3EU2rGh+XYwnARUsgGkvBeFAmWLEXbYdZaeJwYvfOg2y9bmlW7plNjqOhWvyt7hk
-RQIDAQAB
+        """-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAv9hYLdq0XIQO7Rj+hRt7
+fW3G0Cz5WWt1xBU53b/7JwGlAZT/Kw4k76E6MpjVfNqazoXmE3Zjf7Cz+GvXOpzH
+G4pUI4ZBxg2SsQvZTgmpHeKi9Isn0Tt9CC+zObF/7GT9LZwADqIRGNZYwTIzTZqe
+OYyGRPSw9P32faTNZb9wc1EZCrfw4sMPB/RHzxumOc+OE2DtJHwE1ARdsyGQCc4B
+wIiFkvASdpconRhotvhseCu3+XxZqLY8DD2DkYqm8KYErIVXn1GqXnbyQlRRij2O
+n/gXm+j8yxOJBCvPv3LZ0XWbdqYG0a6Ju++oFHYOfTii+tBC+SxMaW/5AA7UbaP7
+cQIDAQAB
 -----END PUBLIC KEY-----""",
-)
+    )
+
+
+rsa_key = _build_key(*_get_pem_key_pair())
