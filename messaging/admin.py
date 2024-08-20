ef9fdc3b858fac59eeee9178ad80d589049cf4c3
Niko Lindroos <niko.j.makela@gmail.com>
2024-08-20 14:19:28 +0300

    feat: add more fields and filters to messaging admin view
    KK-1108

--- messaging/admin.py
+++ messaging/admin.py
@@ -12,13 +12,16 @@ class MessageAdmin(TranslatableAdmin):
     list_display = (
         "subject",
         "project",
+        "protocol",
         "sent_at",
         "get_recipient_count",
         "created_at",
         "updated_at",
+        "get_occurrences_names",
     )
     fields = (
         "project",
+        "protocol",
         "subject",
         "body_text",
         "recipient_selection",
@@ -28,8 +31,24 @@ class MessageAdmin(TranslatableAdmin):
         "updated_at",
         "get_recipient_count",
     )
+    list_filter = ("protocol", "sent_at", "created_at", "updated_at")
     readonly_fields = ("get_recipient_count", "created_at", "updated_at")
+    search_fields = (
+        "translations__subject",
+        "translations__body_text",
+    )
+    date_hierarchy = "created_at"
     actions = ("send",)
+
+    def get_occurrences_names(self, obj):
+        return ", ".join(
+            [
+                f"{occurrence.event}: {occurrence}"
+                for occurrence in obj.occurrences.all()
+            ]
+        )
+
+    get_occurrences_names.short_description = _("occurrences")
 
     def get_recipient_count(self, obj):
         return str(obj.get_recipient_count()) + (
