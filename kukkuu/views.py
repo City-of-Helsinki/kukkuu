424e2c4c91afc148fb9b3e5e32d2b114a3c0049a
Kari Salminen <kari.salminen@anders.com>
2024-11-28 16:01:43 +0200

    fix: sentry_sdk deprecation warning by using current scope
    fix sentry_sdk.configure_scope deprecation warning:
    ```
    DeprecationWarning: sentry_sdk.configure_scope is deprecated and will be
    removed in the next major version. Please consult our migration guide to
    learn how to migrate to the new API:
    https://docs.sentry.io/platforms/python/migration/1.x-to-2.x#scope-configuring
      with sentry_sdk.configure_scope() as scope:
    ```
    by using current scope, see
    https://docs.sentry.io/platforms/python/migration/1.x-to-2.x#scope-configuring
    with the first option i.e.
    "If you want your scope change to affect a smaller unit of code such as
    a span, use the current scope."
    
    refs KK-1357

--- kukkuu/views.py
+++ kukkuu/views.py
@@ -150,12 +150,12 @@ class SentryGraphQLView(FileUploadGraphQLView):
         return result
 
     def _capture_sentry_exceptions(self, errors, query):
-        with sentry_sdk.configure_scope() as scope:
-            scope.set_extra("graphql_query", query)
-            for error in errors:
-                if hasattr(error, "original_error"):
-                    error = error.original_error
-                sentry_sdk.capture_exception(error)
+        scope = sentry_sdk.get_current_scope()
+        scope.set_extra("graphql_query", query)
+        for error in errors:
+            if hasattr(error, "original_error"):
+                error = error.original_error
+            sentry_sdk.capture_exception(error)
 
     @staticmethod
     def format_error(error):
