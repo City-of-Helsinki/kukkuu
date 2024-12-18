4add9aa0fd34d6ba4deabcb0f5150ac3edb6bcbd
Niko Lindroos <niko.j.makela@gmail.com>
2024-12-18 16:46:15 +0200

    test(auditlog-extra): refactor from Django test case to pytest
    KK-1113.
    
    The TestCase usage was requested to be replaced with Pytest formatted
    unit test.

--- hel_django_auditlog_extra/tests/test_mixins.py
+++ hel_django_auditlog_extra/tests/test_mixins.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
-from unittest import mock
 
+import pytest
 from auditlog.context import set_actor
 from auditlog.models import LogEntry
 from auditlog.registry import auditlog
@@ -8,7 +8,7 @@ from django.contrib import admin
 from django.contrib.admin.views.main import ChangeList
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
-from django.test import RequestFactory, TestCase
+from django.test import RequestFactory
 
 from hel_django_auditlog_extra.mixins import AuditlogAdminViewAccessLogMixin
 from hel_django_auditlog_extra.tests.models import DummyTestModel
@@ -18,33 +18,48 @@ User = get_user_model()
 REMOTE_ADDR = "127.0.0.1"
 
 
-class AuditlogAdminViewAccessLogMixinTest(TestCase):
-    def setUp(self):
-        # Register the DummyTestModel to audit logging
-        auditlog.register(DummyTestModel)
+@pytest.fixture(autouse=True)
+def register_auditlog():
+    auditlog.register(DummyTestModel)
+    yield
+    ContentType.objects.clear_cache()
 
-        self.factory = RequestFactory()
-        self.user = User.objects.create_user(
-            username="testuser",
-            password="testpassword",
-            is_staff=True,
-            is_superuser=True,
-        )
-        self.obj1 = DummyTestModel(
-            id=1, text_field="text1", number_field=1, boolean_field=True
-        )
-        self.obj2 = DummyTestModel(
-            id=2, text_field="text2", number_field=2, boolean_field=False
-        )
-        self.all_objects = (self.obj1, self.obj2)
-        self.model_admin = self.get_model_admin(result_list=list(self.all_objects))
 
-    def tearDown(self):
-        # To fix caching issues when running multiple tests at once...
-        ContentType.objects.clear_cache()
-        return super().tearDown()
+@pytest.fixture
+def user():
+    return User.objects.create_user(
+        username="testuser",
+        password="testpassword",
+        is_staff=True,
+        is_superuser=True,
+    )
 
-    def get_model_admin(self, result_list: Optional[List] = None, **kwargs):
+
+@pytest.fixture
+def obj1():
+    return DummyTestModel(id=1, text_field="text1", number_field=1, boolean_field=True)
+
+
+@pytest.fixture
+def obj2():
+    return DummyTestModel(id=2, text_field="text2", number_field=2, boolean_field=False)
+
+
+@pytest.fixture
+def all_objects(obj1, obj2):
+    return (obj1, obj2)
+
+
+@pytest.fixture
+def factory():
+    return RequestFactory()
+
+
+@pytest.fixture
+def model_admin(all_objects):
+    (obj1, obj2) = all_objects
+
+    def get_model_admin(result_list: Optional[List] = None, **kwargs):
         class MockedChangeList(ChangeList):
             show_full_result_count = False
 
@@ -80,12 +95,6 @@ class AuditlogAdminViewAccessLogMixinTest(TestCase):
                 # Mock the result_list of objects.
                 self.result_list = self.get_queryset(request)
 
-            def get_object(self, request, object_id, from_field=None):
-                return self.obj1
-
-            def get_deleted_objects(self, objs, request):
-                return [self.get_object()]
-
         class DummyTestModelAdmin(AuditlogAdminViewAccessLogMixin, admin.ModelAdmin):
             """
             Register DummyTestModel with DummyTestModelAdmin to test
@@ -97,98 +106,96 @@ class AuditlogAdminViewAccessLogMixinTest(TestCase):
             def get_changelist(self, request, **kwargs):
                 return MockedChangeList
 
+            def to_field_allowed(self, request, to_field):
+                return True
+
+            def get_object(self, request, object_id, from_field=None):
+                return obj1
+
+            def get_deleted_objects(self, objs, request):
+                return [
+                    self.get_object(request, obj1.pk),
+                    {},
+                    False,
+                    True,
+                ]
+
         return DummyTestModelAdmin(DummyTestModel, admin.site)
 
-    def assert_access_log(self, log: LogEntry, obj, user=None):
-        if not user:
-            user = self.user
-        self.assertEqual(log.actor, user)
-        self.assertEqual(log.remote_addr, REMOTE_ADDR)
-        self.assertEqual(log.content_type.model, obj._meta.model_name)
-        self.assertEqual(log.object_id, obj.id)
+    return get_model_admin(result_list=list(all_objects))
 
-    def test_changelist_view_logging(self):
-        for is_write_accessed_enabled in (True, False):
-            with self.subTest(write_accessed_from_list_view=is_write_accessed_enabled):
-                auditlog_count_on_start = LogEntry.objects.count()
-                self.model_admin.write_accessed_from_list_view = (
-                    is_write_accessed_enabled
-                )
-                request = self.factory.get(
-                    "/admin/hel_django_auditlog_extra/dummytestmodel/"
-                )
-                request.user = self.user
-                with set_actor(request.user, remote_addr=REMOTE_ADDR):
-                    response = self.model_admin.changelist_view(request)
-                    self.assertEqual(response.status_code, 200)
-                    if is_write_accessed_enabled:
-                        self.assertEqual(
-                            LogEntry.objects.count(),
-                            auditlog_count_on_start + len(self.all_objects),
-                        )
-                    else:
-                        self.assertEqual(
-                            LogEntry.objects.count(), auditlog_count_on_start
-                        )
-                    log = LogEntry.objects.first()
-                    self.assertEqual(log.actor, self.user)
-                    self.assertEqual(log.remote_addr, REMOTE_ADDR)
-                    self.assertEqual(
-                        log.content_type.model, DummyTestModel._meta.model_name
-                    )
 
-    def test_change_view(self):
-        request = self.factory.get(
-            f"/admin/hel_django_auditlog_extra/dummytestmodel/{self.obj1.pk}/change/"
-        )
-        request.user = self.user
-        auditlog_count_on_start = LogEntry.objects.count()
-        with set_actor(request.user, remote_addr=REMOTE_ADDR):
-            with mock.patch.object(
-                self.model_admin,
-                "get_object",
-                return_value=self.obj1,
-            ):
-                response = self.model_admin.change_view(request, str(self.obj1.pk))
-                self.assertEqual(response.status_code, 200)
-                self.assertEqual(LogEntry.objects.count(), auditlog_count_on_start + 1)
+def assert_access_log(log: LogEntry, obj, user):
+    assert log.actor == user
+    assert log.remote_addr == REMOTE_ADDR
+    assert log.content_type.model == obj._meta.model_name
+    assert log.object_id == obj.id
 
-                log = LogEntry.objects.first()
-                self.assert_access_log(log, self.obj1)
 
-    def test_history_view(self):
-        request = self.factory.get(
-            f"/admin/hel_django_auditlog_extra/dummytestmodel/{self.obj1.pk}/history/"
-        )
-        request.user = self.user
-        auditlog_count_on_start = LogEntry.objects.count()
-        with set_actor(request.user, remote_addr=REMOTE_ADDR):
-            with mock.patch.object(
-                self.model_admin,
-                "get_object",
-                return_value=self.obj1,
-            ):
-                response = self.model_admin.history_view(request, str(self.obj1.pk))
-                self.assertEqual(response.status_code, 200)
-                self.assertEqual(LogEntry.objects.count(), auditlog_count_on_start + 1)
+@pytest.mark.django_db
+@pytest.mark.parametrize("is_write_accessed_enabled", (True, False))
+def test_changelist_view_logging(
+    factory, user, model_admin, is_write_accessed_enabled, all_objects
+):
+    auditlog_count_on_start = LogEntry.objects.count()
+    model_admin.write_accessed_from_list_view = is_write_accessed_enabled
+    request = factory.get("/admin/hel_django_auditlog_extra/dummytestmodel/")
+    request.user = user
+    with set_actor(request.user, remote_addr=REMOTE_ADDR):
+        response = model_admin.changelist_view(request)
+        assert response.status_code == 200
+        if is_write_accessed_enabled:
+            assert LogEntry.objects.count() == auditlog_count_on_start + len(
+                all_objects
+            )
+            log = LogEntry.objects.first()
+            assert_access_log(log, all_objects[1], user)
+        else:
+            assert LogEntry.objects.count() == auditlog_count_on_start
 
-                log = LogEntry.objects.first()
-                self.assert_access_log(log, self.obj1)
 
-    def test_delete_view(self):
-        request = self.factory.get(
-            f"/admin/hel_django_auditlog_extra/dummytestmodel/{self.obj1.pk}/delete/"
-        )
-        request.user = self.user
-        auditlog_count_on_start = LogEntry.objects.count()
-        with set_actor(request.user, remote_addr=REMOTE_ADDR):
-            with mock.patch.object(
-                self.model_admin,
-                "get_object",
-                return_value=self.obj1,
-            ):
-                response = self.model_admin.delete_view(request, str(self.obj1.pk))
-                self.assertEqual(response.status_code, 200)
-                self.assertEqual(LogEntry.objects.count(), auditlog_count_on_start + 1)
-                log = LogEntry.objects.first()
-                self.assert_access_log(log, self.obj1)
+@pytest.mark.django_db
+def test_change_view(factory, user, model_admin, obj1):
+    request = factory.get(
+        f"/admin/hel_django_auditlog_extra/dummytestmodel/{obj1.pk}/change/"
+    )
+    request.user = user
+    auditlog_count_on_start = LogEntry.objects.count()
+    with set_actor(request.user, remote_addr=REMOTE_ADDR):
+        response = model_admin.change_view(request, str(obj1.pk))
+        assert response.status_code == 200
+        assert LogEntry.objects.count() == auditlog_count_on_start + 1
+
+        log = LogEntry.objects.first()
+        assert_access_log(log, obj1, user)
+
+
+@pytest.mark.django_db
+def test_history_view(factory, user, model_admin, obj1):
+    request = factory.get(
+        f"/admin/hel_django_auditlog_extra/dummytestmodel/{obj1.pk}/history/"
+    )
+    request.user = user
+    auditlog_count_on_start = LogEntry.objects.count()
+    with set_actor(request.user, remote_addr=REMOTE_ADDR):
+        response = model_admin.history_view(request, str(obj1.pk))
+        assert response.status_code == 200
+        assert LogEntry.objects.count() == auditlog_count_on_start + 1
+
+        log = LogEntry.objects.first()
+        assert_access_log(log, obj1, user)
+
+
+@pytest.mark.django_db
+def test_delete_view(factory, user, model_admin, obj1):
+    request = factory.get(
+        f"/admin/hel_django_auditlog_extra/dummytestmodel/{obj1.pk}/delete/"
+    )
+    request.user = user
+    auditlog_count_on_start = LogEntry.objects.count()
+    with set_actor(request.user, remote_addr=REMOTE_ADDR):
+        response = model_admin.delete_view(request, str(obj1.pk))
+        assert response.status_code == 200
+        assert LogEntry.objects.count() == auditlog_count_on_start + 1
+        log = LogEntry.objects.first()
+        assert_access_log(log, obj1, user)
