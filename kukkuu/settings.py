City-of-Helsinki/kukkuu

commit 2d14cbe34119a8fbce8216ea8f838724ae9a92b1

fix: django-admin logout by not importing helusers SOCIAL_AUTH_PIPELINE
With `from helusers.defaults import SOCIAL_AUTH_PIPELINE  # noqa: F401`
the post logout redirect uri was /helauth/logout/complete/ which didn't
match the /admin/ value given in the service integration document and
configured to Keycloak.

Updating the service integration document and configuring Keycloak to
use the post logout redirect uri of /helauth/logout/complete/ would also
fix this problem, and would log out the user from the AD also AFAIK.

Removing `from helusers.defaults import SOCIAL_AUTH_PIPELINE` makes the
post logout redirect uri match /admin/ i.e. the value configured to
Keycloak, and fixes logout from django-admin. A side effect of this is
that the user is not logged out from the AD.

refs KK-1409

kukkuu/settings.py
@@ -6,7 +6,6 @@ import environ
 import sentry_sdk
 from django.core.exceptions import ImproperlyConfigured
 from django.utils.translation import gettext_lazy as _
-from helusers.defaults import SOCIAL_AUTH_PIPELINE  # noqa: F401
 from jose import ExpiredSignatureError
 from sentry_sdk.integrations.django import DjangoIntegration
 from sentry_sdk.types import Event, Hint

commit e4a994d68693f07877aa3f599e55d0dadb8060ce

fix(keycloak): use helusers.defaults.SOCIAL_AUTH_PIPELINE
Revert 2d14cbe34119a8fbce8216ea8f838724ae9a92b1 which caused bug
https://helsinkisolutionoffice.atlassian.net/browse/KK-1422 i.e.
"AD-login to django-admin & kukkuu-admin UI with same AD user creates
two different users in backend".

Needs updating of post logout redirect URIs at Keycloak from /admin/ to
/helauth/logout/complete/, updated the post logout redirect URIs to
Kukkuu's service integration document:
https://helsinkisolutionoffice.atlassian.net/wiki/spaces/KAN/pages/8679030785/Palveluintegraatio+HP+-+Kukkuu+Kulttuurin+kummilapset

Asked Solution Office (i.e. Ratkaisutoimisto) on 2025-03-04 to update
the post logout redirect URIs to Keycloak.

refs KK-1422

kukkuu/settings.py
@@ -6,6 +6,7 @@ import environ
 import sentry_sdk
 from django.core.exceptions import ImproperlyConfigured
 from django.utils.translation import gettext_lazy as _
+from helusers.defaults import SOCIAL_AUTH_PIPELINE  # noqa: F401
 from jose import ExpiredSignatureError
 from sentry_sdk.integrations.django import DjangoIntegration
 from sentry_sdk.types import Event, Hint