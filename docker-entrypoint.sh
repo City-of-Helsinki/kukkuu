d350de4dc5987e317421c9cc2ff321e847daea4f
Juha Louhiranta <juha.louhiranta@anders.com>
2025-05-13 15:12:10 +0300

    chore: fix issues reported by shellcheck

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # -z for empty / not assigned variable or -o to check whether the value is 0 (=should not be skipped)
-if [ -z "$SKIP_DATABASE_CHECK" -o "$SKIP_DATABASE_CHECK" = "0" ]; then
+if [ -z "$SKIP_DATABASE_CHECK" ] || [ "$SKIP_DATABASE_CHECK" = "0" ]; then
   until nc -z -v -w30 "$DATABASE_HOST" 5432
   do
     echo "Waiting for postgres database connection..."
@@ -25,7 +25,7 @@ if [[ "$ADD_DEFAULT_LANGUAGES" = "1" ]]; then
 fi
 
 # Start server
-if [[ ! -z "$@" ]]; then
+if [[ -n "$*" ]]; then
     "$@"
 elif [[ "$DEV_SERVER" = "1" ]]; then
     python -Wd ./manage.py runserver 0.0.0.0:8081
