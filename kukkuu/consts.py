99834d6764e4b7a1a77a8ea9dd2c452a43459039
Nico Virkki <nico.virkki@anders.com>
2025-10-17 09:28:33 +0300

    deps: upgrade django-csp to version 4
    Refs: KK-1467

--- kukkuu/consts.py
+++ kukkuu/consts.py
@@ -38,16 +38,3 @@ UNAUTHENTICATED_ERROR = "UNAUTHENTICATED_ERROR"
 AUTHENTICATION_ERROR = "AUTHENTICATION_ERROR"
 AUTHENTICATION_EXPIRED_ERROR = "AUTHENTICATION_EXPIRED_ERROR"
 VERIFICATION_TOKEN_INVALID_ERROR = "VERIFICATION_TOKEN_INVALID_ERROR"
-
-
-class CSP:
-    """The “special” source values of 'self', 'unsafe-inline', 'unsafe-eval', 'none'
-    and hash-source ('sha256-...') must be quoted! e.g.: CSP_DEFAULT_SRC = ("'self'",).
-    Without quotes they will not work as intended.
-    Ref. https://django-csp.readthedocs.io/en/stable/configuration.html.
-    """
-
-    SELF = "'self'"
-    NONE = "'none'"
-    UNSAFE_INLINE = "'unsafe-inline'"
-    UNSAFE_EVAL = "'unsafe-eval'"
