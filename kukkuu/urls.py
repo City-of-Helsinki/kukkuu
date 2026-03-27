161885bb898590bf7b7699db39712ed0b4b5c982
Nico Virkki <nico.virkki@anders.com>
2026-03-27 15:35:18 +0200

    feat: use django-helsinki-health-endpoints for readiness and healthz
    django-health-check is replaced by django-helsinki-health-endpoints.
    
    Refs: KEH-242

--- kukkuu/urls.py
+++ kukkuu/urls.py
@@ -2,18 +2,14 @@ from csp.constants import UNSAFE_INLINE
 from csp.decorators import csp_update
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import JsonResponse
 from django.urls import include, path, re_path
 from django.utils.translation import gettext_lazy as _
 from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_http_methods
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView
 from helusers.admin_site import admin
 from rest_framework import routers
 
 from common.utils import get_api_version
-from custom_health_checks.views import HealthCheckJSONView
-from kukkuu import __version__
 from kukkuu.views import SentryGraphQLView
 from reports.api import ChildViewSet, EventGroupViewSet, EventViewSet, VenueViewSet
 
@@ -65,27 +61,8 @@ urlpatterns = [
     path("gdpr-api/", include("helsinki_gdpr.urls")),
 ]
 
-
-#
-# Kubernetes liveness & readiness probes
-#
-
-
-@require_http_methods(["GET", "HEAD"])
-def readiness(*args, **kwargs):
-    response_json = {
-        "status": "ok",
-        "release": settings.APP_RELEASE,
-        "packageVersion": __version__,
-        "commitHash": settings.REVISION,
-        "buildTime": settings.APP_BUILD_TIME.strftime("%Y-%m-%dT%H:%M:%S.000Z"),
-    }
-    return JsonResponse(response_json, status=200)
-
-
 urlpatterns += [
-    path(r"healthz", HealthCheckJSONView.as_view(), name="healthz"),
-    path("readiness", readiness),
+    path("", include("helsinki_health_endpoints.urls")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
